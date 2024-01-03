be15b783a450b856a133dde862ec23dc667eb984
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-01-03 23:45:23 +0100

    Added prettier and working on queue rendering

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,40 @@
+import {
+  HiSolidEllipsisVertical,
+  HiSolidMagnifyingGlass,
+  HiSolidPlay,
+} from "solid-icons/hi";
 import { queue } from "./AudioHandler";
 
 const Header = () => {
   return (
     <>
-      <div class="fixed z-50 top-16 bottom-16 right-0 w-64 bg-red-100">
-        {queue().items.map((item, i) => {
-          if (i === queue().index) {
-            return <p class="text-yellow-400">{item.name}</p>;
-          }
-          return <p>{item.name}</p>;
-        })}
+      <div class="fixed bottom-16 right-0 top-16 z-50 w-64 bg-red-100">
+        <div class="flex flex-col">
+          {queue().items.map((item, i) => {
+            return (
+              <div class="group flex items-center gap-2 border-b-2 border-black bg-red-600 px-2">
+                <div class="relative">
+                  <img
+                    class="object-cover"
+                    src={`http://localhost:3000${item.coverArt}`}
+                    alt=""
+                    width="48"
+                  />
+                  <button class="absolute bottom-0 left-0 right-0 top-0 z-10 hidden w-full items-center justify-center bg-red-400/30 group-hover:flex">
+                    <HiSolidPlay class="text-center" size="32" />
+                  </button>
+                </div>
+                <div class="flex flex-col gap-1">
+                  <p class="text-sm">{item.name}</p>
+                  <p class="text-xs text-gray-500">{item.artistName}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
 
-      <div class="fixed top-0 left-0 right-0 z-50 h-16 border-b-2 bg-white px-4 shadow-lg">
+      <div class="fixed left-0 right-0 top-0 z-50 h-16 border-b-2 bg-white px-4 shadow-lg">
         <div class="flex h-full items-center justify-between">
           <div class="flex h-full items-center gap-4 px-3">
             <a class="text-2xl" href="/">
@@ -21,35 +43,11 @@ const Header = () => {
           </div>
           <div class="flex gap-6">
             <button>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                aria-hidden="true"
-                class="h-8 w-8"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
+              <HiSolidMagnifyingGlass size="32" />
             </button>
             <div class="relative flex items-center">
               <button>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  aria-hidden="true"
-                  class="h-8 w-8"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M10.5 6a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zm0 6a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zm0 6a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
+                <HiSolidEllipsisVertical size="32" />
               </button>
             </div>
           </div>
