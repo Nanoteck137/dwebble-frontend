e0f224cbdef784284af187e5e745b287da051c08
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-01-31 13:16:18 +0100

    Setting up project

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,17 @@
 /* @refresh reload */
-import { render } from 'solid-js/web'
+import { render } from "solid-js/web";
 
-import './index.css'
-import App from './App'
+import { Route, Router } from "@solidjs/router";
+import "./index.css";
+import Home from "./pages/Home";
 
-const root = document.getElementById('root')
+const root = document.getElementById("root");
 
-render(() => <App />, root!)
+render(
+  () => (
+    <Router>
+      <Route path="/" component={Home} />
+    </Router>
+  ),
+  root!,
+);
---
+++ src/pages/Home.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <p class="text-red-200">Home Page</p>;
+};
+
+export default Home;
