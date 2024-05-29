b002a3da6cc7bef95d801f3951e1e95a17311d18
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-05-29 22:19:43 +0200

    Added util to convert api track to music track

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
+import { Track } from "./models/apiGen";
+import { MusicTrack } from "./musicManager";
+
 export function formatTime(s: number) {
   const min = Math.floor(s / 60);
   const sec = Math.floor(s % 60);
 
   return `${min}:${sec.toString().padStart(2, "0")}`;
+}
+
+export function trackToMusicTrack(track: Track): MusicTrack {
+  return {
+    name: track.name,
+    artistName: track.artistName,
+    source: track.mobileQualityFile,
+    coverArt: track.coverArt,
+  };
 }
