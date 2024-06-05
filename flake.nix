1f4df5202fd52b586b7b8a8b33c7aecadc46eb31
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-06-06 01:02:21 +0200

    Added nix build

--- flake.nix
+++ flake.nix
@@ -5,17 +5,38 @@
     nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url  = "github:numtide/flake-utils";
     pyrin.url        = "github:nanoteck137/pyrin/v0.1.1";
+
+    gitignore.url = "github:hercules-ci/gitignore.nix";
+    gitignore.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, pyrin, ... }:
+  outputs = { self, nixpkgs, flake-utils, gitignore, pyrin, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+
+        version = self.shortRev or "dirty";
+
+        app = pkgs.buildNpmPackage {
+          name = "dwebble-frontend";
+          inherit version;
+
+          src = gitignore.lib.gitignoreSource ./.;
+          npmDepsHash = "sha256-3hDm1/M3vgLgREyteMX+dKveW2j7x0dpJZfXSuuNqoE=";
+
+          installPhase = ''
+            runHook preInstall
+            cp -r dist $out/
+            runHook postInstall
+          '';
+        };
       in
       {
+        packages.default = app;
+
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             nodejs
