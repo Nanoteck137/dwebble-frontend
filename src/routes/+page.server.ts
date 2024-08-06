0b01c16a2e3bed6c8ce32125afdb1a4a0e77a080
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-08-06 16:43:22 +0200

    Fixed bug with using the wrong api client again

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,7 @@
-import { ApiClient } from "$lib/api/client";
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async () => {
-  const client = new ApiClient("http://10.28.28.6:3000");
-
-  const tracks = await client.getTracks();
+export const load: PageServerLoad = async ({ locals }) => {
+  const tracks = await locals.apiClient.getTracks();
   if (tracks.status === "error") throw tracks.error.message;
 
   return {
